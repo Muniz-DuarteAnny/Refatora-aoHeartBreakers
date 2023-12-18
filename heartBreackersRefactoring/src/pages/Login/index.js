55517e5e667ccfe4bbf37c35b87457f307bfcf77
DudaLeandra <133047874+DudaLeandra@users.noreply.github.com>
2023-12-18 09:17:49 -0300

    update login

--- heartBreackersRefactoring/src/pages/Login/index.js
+++ heartBreackersRefactoring/src/pages/Login/index.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Importando as bibliotecas necessárias
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native'
 
-export default function Login() {
+import HB from './assets/HB.png'
+
+const LoginScreen = () => {
+  // Estados para armazenar o nome de usuário e senha
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  // Função para lidar com o clique no botão de login
+  const handleLogin = () => {
+    // Aqui você pode adicionar a lógica de autenticação, por exemplo, verificar as credenciais no servidor.
+    // Para este exemplo, apenas imprimiremos as credenciais.
+    console.log('Username:', username)
+    console.log('Password:', password)
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Tela de login</Text>
-      <StatusBar style="auto" />
+      
+      <Image source={HB} style={styles.logo} />
+
+      <Text style={styles.title}>Login</Text>
+
+      {/* Campo de entrada para o nome de usuário */}
+      <TextInput
+        style={styles.input}
+        placeholder="CPF"
+        onChangeText={text => setUsername(text)}
+      />
+
+      {/* Campo de entrada para a senha */}
+      <TextInput
+        style={styles.input}
+        placeholder="SENHA"
+        secureTextEntry
+        onChangeText={text => setPassword(text)}
+      />
+
+
+      {/* Botão de login */}
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
+// Estilos do componente
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10
   },
-});
+  title: {
+    fontSize: 24,
+    marginBottom: 16
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 16,
+    paddingLeft: 8
+  },
+  button: {
+    backgroundColor:'#9a0019',
+    padding: 10,
+    borderRadius: 8
+  },
+  logo: {
+    width: 100, // Ajuste a largura conforme necessário
+    height: 100, // Ajuste a altura conforme necessário
+    marginBottom: 16,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center'
+  }
+})
+
+export default LoginScreen 
