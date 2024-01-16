22d831f5c592b3d3f318462ae1542173474509fc
Maísa <maisaresende00@gmail.com>
2024-01-15 22:48:34 -0300

    tela de login

--- heartBreackersRefactoring/src/pages/Login/index.js
+++ heartBreackersRefactoring/src/pages/Login/index.js
@@ -1,96 +1,117 @@
-// Importando as bibliotecas necessárias
-import React, { useState } from 'react'
+
+import React, { useState } from 'react';
 import {
   View,
   Text,
   TextInput,
   Image,
   TouchableOpacity,
-  StyleSheet
-} from 'react-native'
+  StyleSheet,
+  ImageBackground
+} from 'react-native';
 
-import HB from './assets/HB.png'
+import HB from './assets/logoHB.png';
 
 const LoginScreen = () => {
-  // Estados para armazenar o nome de usuário e senha
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
 
-  // Função para lidar com o clique no botão de login
-  const handleLogin = () => {
-    // Aqui você pode adicionar a lógica de autenticação, por exemplo, verificar as credenciais no servidor.
-    // Para este exemplo, apenas imprimiremos as credenciais.
-    console.log('Username:', username)
-    console.log('Password:', password)
-  }
   
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  
+  const handleLogin = () => {
+    // verificar as credenciais no servidor.
+  
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
   return (
-    <View style={styles.container}>
-      
-      <Image source={HB} style={styles.logo} />
+    <ImageBackground source={require('./assets/background3.jpg')} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Image source={HB} style={styles.logo} />
+        <Text style={styles.title}>BEM VINDO!</Text>
 
-      <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="CPF"
+          placeholderTextColor={"white"}
+          onChangeText={text => setUsername(text)}
+        />
 
-      {/* Campo de entrada para o nome de usuário */}
-      <TextInput
-        style={styles.input}
-        placeholder="CPF"
-        onChangeText={text => setUsername(text)}
-      />
+        <TextInput
+          style={styles.input}
+          placeholder="SENHA"
+          placeholderTextColor={"white"}
+          secureTextEntry
+          onChangeText={text => setPassword(text)}
+        />
 
-      {/* Campo de entrada para a senha */}
-      <TextInput
-        style={styles.input}
-        placeholder="SENHA"
-        secureTextEntry
-        onChangeText={text => setPassword(text)}
-      />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
 
 
-      {/* Botão de login */}
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
-
-// Estilos do componente
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', 
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 10
+    padding: 20, 
   },
   title: {
-    fontSize: 24,
-    marginBottom: 16
+    fontSize: 30,
+    marginBottom: 16,
+    color:"white",
+    marginBottom: 20
   },
   input: {
     height: 40,
     width: '100%',
-    borderColor: 'gray',
+    borderColor: '#ffff',
     borderWidth: 1,
     borderRadius: 8,
     marginBottom: 16,
-    paddingLeft: 8
+    paddingLeft: 8,
+    backgroundColor: 'rgba(255, 255, 255, 0.40)',
+    marginBottom: 30
   },
   button: {
-    backgroundColor:'#9a0019',
+    backgroundColor: '#9a0019',
     padding: 10,
-    borderRadius: 8
+    paddingLeft: 70,
+    paddingRight: 70,
+    borderRadius: 8,
+    borderColor: "white",
+    borderWidth: 0.5,
   },
   logo: {
-    width: 100, // Ajuste a largura conforme necessário
-    height: 100, // Ajuste a altura conforme necessário
-    marginBottom: 16,
+    width: 100,
+    height: 100,
+    marginBottom: 60,
+    resizeMode:"stretch"
   },
   buttonText: {
     color: 'white',
-    fontSize: 16,
-    textAlign: 'center'
-  }
-})
+    fontSize: 18,
+    textAlign: 'center',
+    
+   
+  },
 
-export default LoginScreen 
+  
+});
+
+
+
+
+export default LoginScreen;
